e409d437c27f573f3ad41fa9fffda6fce16d17d0
yilmazkusatmer <kusatmer@googlemail.com>
2021-12-26 22:08:19 +0100

    refactoring test

--- math/perfectnumber/perfectnumber_test.go
+++ math/perfectnumber/perfectnumber_test.go
@@ -10,17 +10,17 @@ var Tests = []struct {
 	input  int
 	result []int
 }{
-	{"perfectnumber6", 6, []int{6}},
-	{"perfectnumber1000", 1000, []int{6, 28, 496}},
-	{"perfectnumber10000", 10000, []int{6, 28, 496, 8128}},
+	{"calculate perfectnumber for 6", 6, []int{6}},
+	{"calculate perfectnumber for 1000", 1000, []int{6, 28, 496}},
+	{"calculate perfectnumber for 10000", 10000, []int{6, 28, 496, 8128}},
 }
 
 func TestCalcPerferctNumbers(t *testing.T) {
 	for _, tt := range Tests {
 		t.Run(tt.name, func(t *testing.T) {
-			calcResult := CalcPerfectNumbers(tt.input)
-			if !reflect.DeepEqual(calcResult, tt.result) {
-				t.Errorf("Calc(%v) produce %v, but expected %v", 1000, calcResult, tt.result)
+			perfectNumbers := CalcPerfectNumbers(tt.input)
+			if !reflect.DeepEqual(perfectNumbers, tt.result) {
+				t.Errorf("CalcPerfectNumbers(%v) produce %v, but expected %v", tt.input, perfectNumbers, tt.result)
 			}
 		})
 	}
